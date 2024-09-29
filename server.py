0140ef84d27a5bc5748d5638a969a3446be5d95a
Ayush Gharat <gharatayush27@gmail.com>
2024-09-28 22:29:31 -0400

    Added gitignore

--- server.py
+++ server.py
@@ -3,20 +3,10 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # Define an endpoint
-@app.route('/api/hello', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello_world():
     return jsonify(message="Hello, World!")
 
-# Another endpoint with a parameter
-@app.route('/api/greet/<name>', methods=['GET'])
-def greet(name):
-    return jsonify(message=f"Hello, {name}!")
-
-# POST endpoint example
-@app.route('/api/data', methods=['POST'])
-def receive_data():
-    data = request.json
-    return jsonify(received_data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
